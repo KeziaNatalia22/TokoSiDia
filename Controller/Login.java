a70dfee05882f03984909fa8fb588b2b7e8e53b9
garrychandra <garryalexanderchandra97@gmail.com>
2024-12-20 12:18:35 +0700

    update login

--- Controller/Login.java
+++ Controller/Login.java
@@ -5,27 +5,36 @@ import java.sql.ResultSet;
 import javax.swing.JOptionPane;
 
 public class Login {
-    public static void loginTokoSidia(String username, String password){
-        String query = "select * from users where username = ? and password = ?";
-        try{
+    public static void loginTokoSidia(String username, String password) {
+        String query = "select * from user where username = ?";
+        try {
             PreparedStatement st = DatabaseHandler.connect().prepareStatement(query);
-
             st.setString(1, username);
-            st.setString(2, password);
 
             ResultSet rs = st.executeQuery();
 
-            if (rs != null) {
-                JOptionPane.showMessageDialog(null, "Login Berhasil", "Login", JOptionPane.DEFAULT_OPTION);
-                if (rs.getString("type").equalsIgnoreCase("seller")) {
-                    // View.HomeSeller();
+            if (rs.next()) {
+                query = "select * from user where username = ? and password = ?";
+                st = DatabaseHandler.connect().prepareStatement(query);
+                st.setString(1, username);
+                st.setString(2, password);
+
+                rs = st.executeQuery();
+                if (rs.next()) {
+                    JOptionPane.showMessageDialog(null, "Login Berhasil", "Login", JOptionPane.DEFAULT_OPTION);
+                    
+                    if (rs.getString("type").equalsIgnoreCase("seller")) {
+                        // View.HomeSeller();
+                    } else {
+                        // View.HomeBuyer();
+                    }
+                } else {
+                    JOptionPane.showMessageDialog(null, "Login Gagal\nPassword Salah", "Login Gagal",
+                            JOptionPane.DEFAULT_OPTION);
                 }
-                else{
-                    // View.HomeBuyer();
-                }
-            }
-            else{
-                JOptionPane.showMessageDialog(null, "Login Gagal", "Login", JOptionPane.DEFAULT_OPTION);
+            } else {
+                JOptionPane.showMessageDialog(null, "Login Gagal\nUsername not found", "Login Gagal",
+                        JOptionPane.DEFAULT_OPTION);
                 // View.Login();
             }
         } catch (Exception e) {
@@ -33,8 +42,8 @@ public class Login {
         }
     }
 
-    public static void forgotPassword(){
-        
+    public static void forgotPassword() {
+
     }
-    
+
 }
