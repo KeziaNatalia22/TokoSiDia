57e6d8b21d57e06f9179a28df0972ddc8fbccdc8
1123001 - Kezia Natalia <163112818+Kezia2212@users.noreply.github.com>
2025-01-02 13:31:14 +0700

    transaksi history buyer

--- Modul/Transaction.java
+++ Modul/Transaction.java
@@ -1,22 +1,32 @@
 package Modul;
-
+import java.util.Date;
 import java.util.ArrayList;
 
 public class Transaction {
     private ArrayList<Product> listProduct = new ArrayList<Product>();
     private String shopName, buyerName;
     private ShipmentStatus_Enum shipmentStatus;
+    private Date transactionDate;
+
+    public Date getTransactionDate() {
+        return transactionDate;
+    }
+
+    public void setTransactionDate(Date transactionDate) {
+        this.transactionDate = transactionDate;
+    }
 
     public Transaction(){
         
     }
     
     public Transaction(ArrayList<Product> listProduct, String shopName, String buyerName,
-            ShipmentStatus_Enum shipmentStatus) {
+            ShipmentStatus_Enum shipmentStatus, Date transactionDate) {
         this.listProduct = listProduct;
         this.shopName = shopName;
         this.buyerName = buyerName;
         this.shipmentStatus = shipmentStatus;
+        this.transactionDate = transactionDate;
     }
     
     public ArrayList<Product> getListProduct() {
